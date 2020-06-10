c166c975486df464fc65427e6c9059d5d72c739d
Sam Barrick <you@example.com>
2020-06-09 21:33:03 -0500

    added leaflet map feature. Need to put it in a separate component, but it is functional

---
+++ src/components/InteractiveMap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const InteractiveMap = (props) => {
+  
+    return (
+        <div>
+    
+        </div>
+
+    )
+}
+
+export default InteractiveMap;
--- src/components/MapPhoto.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MapPhoto = (props) => {
-
-}
-
-export default MapPhoto;
